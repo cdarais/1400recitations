b0fb1d62dcd2722aeffdc5a4914707c8a937f832
cdarais <christopherdarais@icloud.com>
2017-09-16 19:50:11 -0700

    516 files

---
+++ fa17-516/files.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	
+	int counter = 0,
+		temp = 0,
+		total = 0,
+		totalCount = 0;
+
+
+	ifstream fin;
+	fin.open("data/input");
+
+	while (counter < 10){
+		fin >> temp;
+		total += temp;
+		counter++;
+	}
+
+	fin.close();
+
+	cout << total << endl;
+	
+	return 0;
+}
---
+++ fa17-516/variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int amountOfBooks = 8;
+	
+	cout << amountOfBooks << endl;
+
+	amountOfBooks += 2;
+
+	cout << amountOfBooks << endl;
+
+	amountOfBooks = amountOfBooks * 2;
+
+	cout << amountOfBooks << endl;
+
+	cout << amountOfBooks * amountOfBooks << endl;
+
+	return 0;
+}
