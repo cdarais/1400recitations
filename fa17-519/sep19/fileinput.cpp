1ea7b8d35eaa2f644ec57cb8cd7d42333758509d
cdarais <christopherdarais@icloud.com>
2017-09-19 12:28:54 -0700

    class sep 19

---
+++ fa17-519/sep19/fileinput.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int addCurrentAndTotal(int a, int b){
+	return 0;
+}
+
+int addAandB(int a, int b){
+	int	c = a + b;
+	return 0;
+}
+
+int main(){
+/*
+	ifstream fin;
+	int number, count = 0, total = 0;
+
+
+	fin.open("input");
+	cout << "starting file input\n";
+	while (fin >> number){
+		total += number; //total = total + number
+		cout << ++count << " - " << number << endl;
+	}
+	cout << "ending file input\n";
+	cout << total << endl;
+
+	fin.close();
+*/
+
+	int a = 12, b = 15, result = 0, c = 5678;
+	result = addAandB(a, b);
+	cout << result << endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
